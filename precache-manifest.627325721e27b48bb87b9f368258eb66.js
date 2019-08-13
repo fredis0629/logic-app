03fefe22bf7dc8eb9b6ed6c9a6b4228db454a3f8
fredis0629 <fredis0629@gmail.com>
2019-08-13 15:27:47 +0300

    Updates

---
+++ precache-manifest.627325721e27b48bb87b9f368258eb66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ac3b299402bbb7ae9a78e388672a60",
+    "url": "/logic-app/index.html"
+  },
+  {
+    "revision": "6e3b5f8a0fe5b2426951",
+    "url": "/logic-app/static/css/main.4011c4c8.chunk.css"
+  },
+  {
+    "revision": "626cf2bed8ea787dcd60",
+    "url": "/logic-app/static/js/2.05dfdb23.chunk.js"
+  },
+  {
+    "revision": "6e3b5f8a0fe5b2426951",
+    "url": "/logic-app/static/js/main.0ba47358.chunk.js"
+  },
+  {
+    "revision": "f9602dc50e91983a6960",
+    "url": "/logic-app/static/js/runtime~main.d2c20982.js"
+  }
+]);
